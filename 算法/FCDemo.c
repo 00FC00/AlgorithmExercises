9e00d24c56fd3d6ad1f8da971607645a004697e4
FC的MacbookPro <595240129@qq.com>
2020-09-04 18:32:04 +0800

    堆栈思路，分治解决大数运算

--- 算法/FCDemo.c
+++ 算法/FCDemo.c
@@ -8,6 +8,7 @@
 
 #include "FCDemo.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  【枚举法】
@@ -250,13 +251,80 @@ void getFill(char *a, char *b, int ia, int ja, int ib, int jb, int tbool, int mo
         r = pr - move - 1;
         
         for (s=jb,j=0; s>=ib; r--,s--) {
-            <#statements#>
+            n = b[s] - 48;
+            t = m * n + j + result[r];
+            result[r] = t%10;
+            j = t/10;
         }
         
+        for (; j; r--) {
+            t = j + result[r];
+            result[r] = t%10;
+            j = t/10;
+        }
     }
 }
 
+int get(char *a, char *b,int ia, int ja, int ib, int jb, int t, int move) {
+    int m,n,s,j;
+    if (ia==ja) {
+        getFill(a, b, ia, ja, ib, jb, t, move);
+        return 1;
+    } else if (ib==jb) {
+        getFill(b, a, ib, jb, ia, ja, t, move);
+        return 1;
+    } else {
+        m = (ja + ia)/2;
+        n = (jb + ib)/2;
+        s = ja - m;
+        j = jb - n;
+        get(a, b, ia, m, ib, n, t, s+j+move);
+        get(a, b, ia, m, n+1, jb, 0, s+move);
+        get(a, b, m+1, ja, ib, n, 0, j+move);
+        get(a, b, m+1, ja, n+1, jb, 0, 0+move);
+    }
+    return 0;
+}
 
 void largeNumbersMultiply(void) {
+    char *a,*b;
+    int n,flag;
     
+    a = (char *)malloc(1000);
+    b = (char *)malloc(1000);
+    
+    printf("The program will computer a*b\n");
+    printf("Enter a b:");
+    scanf("%s %s", a, b);
+    result = (char *)malloc(strlen(a) + strlen(b) + 2);
+    flag = pr = 1;
+    result[0] = '\0';
+    if (a[0]=='-' && b[0]=='-')
+        get(a, b, 1, (int)strlen(a)-1, 1, (int)strlen(b)-1, 1, 0);
+    
+    if (a[0]=='-' && b[0]!='-') {
+        flag = 0;
+        get(a, b, 1, (int)strlen(a)-1, 0, (int)strlen(b)-1, 1, 0);
+    }
+    
+    if (a[0]!='-' && b[0]=='-') {
+        flag = 0;
+        get(a, b, 0, (int)strlen(a)-1, 1, (int)strlen(b)-1, 1, 0);
+    }
+    
+    if (a[0]!='-' && b[0]!='-')
+        get(a, b, 0, (int)strlen(a)-1, 0, (int)strlen(b)-1, 1, 0);
+    
+    if (!flag) printf("-");
+    
+    if (result[0]) printf("%d", result[0]);
+    
+    for (n=1; n<pr; n++)
+        printf("%d", result[n]);
+    
+    printf("\n");
+    free(a);
+    free(b);
+    free(result);
+    //system("pause");
 }
