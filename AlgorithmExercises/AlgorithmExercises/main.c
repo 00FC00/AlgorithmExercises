aa73961c1a34072860a5557faa0c3906aa3a8c0b
哲思 <595240129@qq.com>
2024-07-23 17:07:17 +0800

    创建Xcode工程，配置练习C环境

---
+++ AlgorithmExercises/AlgorithmExercises/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  AlgorithmExercises
+//
+//  Created by 哲思 on 2024/7/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
